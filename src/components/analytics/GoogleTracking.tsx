02e132e178bf75b1c85e2ec7067b85b9a1d18ddf
fullstack001 <mr.dimas332@gmail.com>
2026-04-28 12:42:51 -0500

    update gtm

--- src/components/analytics/GoogleTracking.tsx
+++ src/components/analytics/GoogleTracking.tsx
@@ -5,28 +5,25 @@ import Script from "next/script";
 declare global {
   interface Window {
     dataLayer: unknown[];
-    gtag?: (...args: unknown[]) => void;
   }
 }
 
-const GA_MEASUREMENT_ID = process.env.NEXT_PUBLIC_GA_MEASUREMENT_ID;
+const GTM_ID = process.env.NEXT_PUBLIC_GTM_ID;
 const GOOGLE_ADS_ID = process.env.NEXT_PUBLIC_GOOGLE_ADS_ID;
-
-const trackingIds = [GA_MEASUREMENT_ID, GOOGLE_ADS_ID].filter(
-  (id): id is string => Boolean(id)
-);
-
-const hasTracking = trackingIds.length > 0;
 
 export function trackEvent(
   eventName: string,
   params: Record<string, string | number | boolean> = {}
 ) {
-  if (typeof window === "undefined" || typeof window.gtag !== "function") {
+  if (typeof window === "undefined") {
     return;
   }
 
-  window.gtag("event", eventName, params);
+  window.dataLayer = window.dataLayer || [];
+  window.dataLayer.push({
+    event: eventName,
+    ...params,
+  });
 }
 
 export function trackAdsConversion(
@@ -38,32 +35,38 @@ export function trackAdsConversion(
     return;
   }
 
-  trackEvent("conversion", {
+  trackEvent("ads_conversion", {
     send_to: `${GOOGLE_ADS_ID}/${conversionLabel}`,
     ...(typeof value === "number" ? { value, currency } : {}),
   });
 }
 
 export default function GoogleTracking() {
-  if (!hasTracking) {
+  if (!GTM_ID) {
     return null;
   }
 
   return (
     <>
-      <Script
-        src={`https://www.googletagmanager.com/gtag/js?id=${trackingIds[0]}`}
-        strategy="afterInteractive"
-      />
-      <Script id="google-tracking-init" strategy="afterInteractive">
+      <Script id="google-tag-manager-init" strategy="afterInteractive">
         {`
           window.dataLayer = window.dataLayer || [];
-          function gtag(){dataLayer.push(arguments);}
-          window.gtag = gtag;
-          gtag('js', new Date());
-          ${trackingIds.map((id) => `gtag('config', '${id}');`).join("\n")}
+          (function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
+          new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
+          j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
+          'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
+          })(window,document,'script','dataLayer','${GTM_ID}');
         `}
       </Script>
+      <noscript>
+        <iframe
+          src={`https://www.googletagmanager.com/ns.html?id=${GTM_ID}`}
+          height="0"
+          width="0"
+          style={{ display: "none", visibility: "hidden" }}
+          title="gtm"
+        />
+      </noscript>
     </>
   );
 }
